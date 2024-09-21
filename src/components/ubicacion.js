669307ed46e21221714c6f818eae74e78aa27199
FrancoRegg <francoreggiardo97@gmail.com>
2024-09-21 18:47:01 +0200

    Incorporacion de ubicacion, imagen del clima y temperatura

--- src/components/ubicacion.js
+++ src/components/ubicacion.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from "react";
 export const Ubicacion = () => {
 const [coords, setCoords] = useState({latitud: null, longitud: null})
 const [datos, setDatos] = useState(null)
+const [localizacion, setLocalizacion] = useState(null)
 
   const options = {
     enableHighAccuracy: true, //Entrega las mejores coordenadas posibles.
@@ -22,13 +23,25 @@ const [datos, setDatos] = useState(null)
     navigator.geolocation.getCurrentPosition(success, error, options);
   }, [])
 
-
-  //Obtengo todo tipos de datos desde la API
-  const datosClima = async () => { 
+  //Obtengo geolocalizacion inversa
+  const geoInversa = async () => {
+    const ubiAPI = `https://us1.locationiq.com/v1/reverse?key=pk.bb957299e7910f56a63a888ddb2f7992&lat=${coords.latitud}&lon=${coords.longitud}&format=json`
     if(coords.latitud && coords.longitud){
-    const URL = `https://api.weatherapi.com/v1/forecast.json?key=a6e61a741ad94b46a0d173802242009 &q=${coords.latitud},${coords.longitud}&days=7&aqi=yes&alerts=yes`
     try{
-      const respuesta = await fetch (URL)
+      const respuesta = await fetch(ubiAPI)
+      const data = await respuesta.json()
+      setLocalizacion(data)
+      console.log("Ubicacion", data);
+    }catch(error){
+      console.log("La localizacion no existe", error);
+    }}
+  }
+  //Obtengo todo tipos de datos desde weatherAPI
+  const datosClima = async () => { 
+    const datosApi = `https://api.weatherapi.com/v1/forecast.json?key=a6e61a741ad94b46a0d173802242009&q=${coords.latitud},${coords.longitud}&days=7&aqi=yes&alerts=yes`
+    if(coords.latitud && coords.longitud){
+    try{
+      const respuesta = await fetch (datosApi)
       const data = await respuesta.json()
       setDatos(data)
       console.log("Datos", data);
@@ -43,6 +56,7 @@ const [datos, setDatos] = useState(null)
 
   useEffect(()=>{ 
     if(coords.latitud && coords.longitud){
+      geoInversa()
       datosClima()
     }
   }, [coords])
@@ -50,7 +64,11 @@ const [datos, setDatos] = useState(null)
   return (
     <>
     <h1>Mi ubicación</h1>
-    
+    {localizacion ? localizacion.address.city : <p>Cargando...</p>}
+    <div className="temperatura">
+      <img className="temperatura_img" src={datos.current.condition.icon} alt=""/>
+      <h3 className="temperatura_actual">{datos.current.temp_c}</h3>
+    </div>
     </>
   )
 }
