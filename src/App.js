e8f2d14ba03a0e2ebc8d251748e261d29c99d769
FrancoRegg <francoreggiardo97@gmail.com>
2024-09-21 09:39:17 +0200

    Incorporo a mi ruta de inicio el componente clima

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Ubicacion } from './components/ubicacion';
+import { Clima } from './components/clima';
 const App = () => {
   
   return (
     <div className="App">
       <h1>Aca empieza la APP del clima</h1>
       <Ubicacion /> 
+      <h2>Pruebas de la API Climatica</h2>
+      <Clima />
     </div>
   );
 }
